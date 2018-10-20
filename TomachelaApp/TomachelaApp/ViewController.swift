f92503eb476d1c062449a100b1c9049f8c98230e
Oliver Jordy Pérez Escamilla <OliverPerez@MacBook-9.local>
2018-10-20 09:52:29 -0500

    Create Xcode project

---
+++ TomachelaApp/TomachelaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TomachelaApp
+//
+//  Created by Oliver Jordy Pérez Escamilla on 20/10/18.
+//  Copyright © 2018 Oliver Jordy Pérez Escamilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
