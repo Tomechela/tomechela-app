c9f46c9ccc60af77d88c6dfcd61a5a44bbb3f8b0
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-10-23 00:08:48 -0500

    adding real names of beers, I modified the constant array beers

--- TomachelaApp/TomachelaApp/Product.swift
+++ TomachelaApp/TomachelaApp/Product.swift
@@ -10,31 +10,28 @@ import UIKit
 
 class Product: NSObject {
     var name: String
-    var valueInDollars: Int
     var category: String
+    var valueInDollars: Int
     
     // A designated initializer is a primary initializer for the class.
     // Every class has at least one designated initializer.
     
     init(name: String, category: String, valueInDollars: Int) {
         self.name = name
-        self.valueInDollars = valueInDollars
         self.category = category
+        self.valueInDollars = valueInDollars
         super.init()
     }
     // convenience initializers are optional.
     convenience init(random: Bool = false) {
         if random {
-            let adjectives = ["beer good", "beer light", "beer lagger"]
-            let nouns = ["Levanta Muertos", "VivaLaVida", "Wera"]
-            var idx = arc4random_uniform(UInt32(adjectives.count))
-            let randomAdjective = adjectives[Int(idx)]
-            idx = arc4random_uniform(UInt32(nouns.count))
-            let randomNoun = nouns[Int(idx)]
-            let randomName = "\(randomAdjective) \(randomNoun)"
+            let beers = ["William Street Beer", "Birra Levante", "Ippon Matsu Beer", "TerraCotta", "Finísima", "Staropramen Lager – edición limitada", "Dolina – Moruba"]
+            let idx = arc4random_uniform(UInt32(beers.count))
+            let randomBeer = beers[Int(idx)]
+            let randomNameOfBeer = "\(randomBeer)"
             let randomValue = Int(arc4random_uniform(50))
             let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
-            self.init(name: randomName, category: randomSerialNumber, valueInDollars: randomValue)
+            self.init(name: randomNameOfBeer, category: randomSerialNumber, valueInDollars: randomValue)
         } else {
             self.init(name: "", category: "", valueInDollars: 0)
         }
